5d8de1e121593cd77736127edeed1bec2206a43d
Theodore Tinker_1 <theodore-tinker@deigo-login1.oist.jp>
2024-01-20 20:21:10 +0900

    okay episode plotting

--- plotting_episodes.py
+++ plotting_episodes.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-from utils import print, args, duration, load_dicts, multihots_to_string, onehots_to_string
+from utils import print, args, duration, load_dicts, multihots_to_string, onehots_to_string, action_to_string
 print("name:\n{}".format(args.arg_name))
 
 def plot_episodes(complete_order, plot_dicts):
@@ -14,72 +14,54 @@ def plot_episodes(complete_order, plot_dicts):
                         plot_episode(key, episode_dict, arg_name)
                         
 def plot_episode(key, episode_dict, arg_name):
-    print("\n\n")
-    print(arg_name)
     agent_num, epoch, episode_num = key.split("_")
-    
+
     steps = len(episode_dict["objects"])
-    fig, axs = plt.subplots(steps, 8)
-    fig.suptitle("Epoch {}".format(epoch))
-    
+    fig, axs = plt.subplots(steps, 8, figsize=(20, 2 * steps))
+    fig.suptitle(f"Epoch {epoch}")
+
     for step in range(steps):
-        
-        ax = axs[step, 0]
-        objects = episode_dict["objects"][step]
-        print("objects", objects.shape)
-        print(objects)
-        ax.text(0, 1, objects)
-        ax.text(0, 0, multihots_to_string(objects))
-        
-        ax = axs[step, 1]
-        comms = episode_dict["comms"][step]
-        print("comms", comms.shape)
-        print(comms)
-        ax.text(0, 1, comms)
-        ax.text(0, 0, comms)
-        
-        if step+1 !=  steps:
-            ax = axs[step, 2]
-            actions = episode_dict["actions"][step]
-            print("actions", actions.shape)
-            print(actions)
-            ax.text(0, 0, actions)
-            
-            ax = axs[step, 3]
-            rewards = episode_dict["rewards"][step]
-            print("rewards")
-            print(rewards)
-            ax.text(0, 0, rewards)
-            
-            ax = axs[step, 4]
-            objects_p = episode_dict["prior_predicted_objects"][step]
-            print("objects_p", objects_p.shape)
-            print(objects_p)
-            ax.text(0, 1, objects_p)
-            ax.text(0, 0, multihots_to_string(episode_dict["prior_predicted_objects"][step]))
-            
-            ax = axs[step, 5]
-            comms_p = episode_dict["prior_predicted_comms"][step]
-            print("comms_p", comms_p.shape)
-            print(comms_p)
-            ax.text(0, 1, comms_p)
-            ax.text(0, 0, onehots_to_string(episode_dict["prior_predicted_comms"][step]))
-            
-            ax = axs[step, 6]
-            objects_q = episode_dict["posterior_predicted_objects"][step]
-            print("objects_q", objects_q.shape)
-            print(objects_q)
-            ax.text(0, 1, objects_q)
-            ax.text(0, 0, multihots_to_string(episode_dict["posterior_predicted_objects"][step]))
-            
-            ax = axs[step, 7]
-            comms_q = episode_dict["posterior_predicted_comms"][step]
-            print("comms_q", comms_q.shape)
-            print(comms_q)
-            ax.text(0, 1, comms_q)
-            ax.text(0, 0, onehots_to_string(episode_dict["posterior_predicted_comms"][step]))
+        for ax_idx in range(8):
+            ax = axs[step, ax_idx]
+            ax.axis('off')  
+
+            if ax_idx == 0:
+                objects = episode_dict["objects"][step]
+                text = multihots_to_string(objects)
+            elif ax_idx == 1:
+                comms = episode_dict["comms"][step]
+                text = onehots_to_string(comms)
+            elif step + 1 != steps:
+                if ax_idx == 2:
+                    actions = episode_dict["actions"][step]
+                    text = action_to_string(actions)
+                elif ax_idx == 3:
+                    rewards = episode_dict["rewards"][step]
+                    text = str(rewards)
+                elif ax_idx == 4:
+                    objects_p = episode_dict["prior_predicted_objects"][step]
+                    text = multihots_to_string(objects_p)
+                elif ax_idx == 5:
+                    comms_p = episode_dict["prior_predicted_comms"][step]
+                    text = onehots_to_string(comms_p)
+                elif ax_idx == 6:
+                    objects_q = episode_dict["posterior_predicted_objects"][step]
+                    text = multihots_to_string(objects_q)
+                elif ax_idx == 7:
+                    comms_q = episode_dict["posterior_predicted_comms"][step]
+                    text = onehots_to_string(comms_q)
+            else:
+                text = ''
                 
-    plt.savefig("{}/epoch_{}_episode_{}_agent_{}.png".format(arg_name, epoch, episode_num, agent_num))
+            text = text.replace('\t', ' ')
+            text = text.replace('(', ' ')
+            text = text.replace(')', ' ')
+
+            # Add text to the subplot with a buffer for margins and word wrapping
+            ax.text(0.1, 0.5 - .2 * ax_idx, text, va='center', ha='left', wrap=True)
+
+    plt.tight_layout(rect=[0, 0, 1, 0.95])  # Adjust layout to prevent overlap
+    plt.savefig(f"{arg_name}/epoch_{epoch}_episode_{episode_num}_agent_{agent_num}.png")
     plt.close()
 
 plot_dicts, min_max_dict, complete_order = load_dicts(args)
